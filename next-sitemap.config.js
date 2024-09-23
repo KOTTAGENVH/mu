940dd00df7a9c0108e6f55b76688b310074dea44
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-09-23 23:16:46 +0530

    feat:sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    siteUrl: 'https://mu.nowenkottage.com',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,       
+    changefreq: 'daily',     
+    priority: 0.7,          
+    exclude: [               
+      '/secret-page',
+      '/admin/*'
+    ],
+  };
+  
