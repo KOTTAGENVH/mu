47a7d4c9ac831de32a5183cc85686e722999bcd8
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2025-02-26 20:32:39 +0530

    Update route.ts

--- mu/src/app/api/services/generateURLToken/route.ts
+++ mu/src/app/api/services/generateURLToken/route.ts
@@ -15,6 +15,7 @@ export async function POST(req: Request) {
     }
     const email = process.env.NEXT_PUBLIC_EMAIL || "";
     const email2 = process.env.NEXT_PUBLIC_EMAIL2 || "";
+    const overallEmail = email + "," + email2;
     const subject = process.env.NEXT_PUBLIC_SUBJECT || "";
     if (!email) {
       throw new Error("EMAIL environment variable is not set.");
@@ -42,7 +43,7 @@ export async function POST(req: Request) {
 
     // Send the token to the user via email
     await customEmail(
-      [email, email2],
+      overallEmail,
       "Token for MU",
       `Please click on the link to login: ${loginLink} @${ip}`
     );
