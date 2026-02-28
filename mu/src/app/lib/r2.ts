cc0c08bb11d72b7cf75d3d4447121783d0d9d76d
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2026-02-28 16:33:58 +0530

    fix: minor fixes

--- mu/src/app/lib/r2.ts
+++ mu/src/app/lib/r2.ts
@@ -1,10 +1,15 @@
-import { S3Client} from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
 //validate env variables
-if (!process.env.CLOUDFLARE_ACCOUNT_ID ||
-    !process.env.CLOUDFLARE_ACCESS_KEY_ID ||
-    !process.env.CLOUDFLARE_SECRET_ACCESS_KEY ||
-    !process.env.R2_BUCKET_NAME) {
+if (
+  !process.env.CLOUDFLARE_ACCOUNT_ID ||
+  !process.env.CLOUDFLARE_ACCESS_KEY_ID ||
+  !process.env.CLOUDFLARE_SECRET_ACCESS_KEY ||
+  !process.env.R2_BUCKET_NAME ||
+  !process.env.CLOUDFLARE_IMG_ACCESS_KEY_ID ||
+  !process.env.CLOUDFLARE_IMG_SECRET_ACCESS_KEY ||
+  !process.env.R2_IMG_BUCKET_NAME
+) {
   throw new Error("Missing required Cloudflare R2 environment variables");
 }
 
@@ -14,5 +19,14 @@ export const s3Client = new S3Client({
   credentials: {
     accessKeyId: process.env.CLOUDFLARE_ACCESS_KEY_ID || "",
     secretAccessKey: process.env.CLOUDFLARE_SECRET_ACCESS_KEY || "",
+  },
+});
+
+export const s3Client2 = new S3Client({
+  region: "auto",
+  endpoint: `https://${process.env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  credentials: {
+    accessKeyId: process.env.CLOUDFLARE_IMG_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.CLOUDFLARE_IMG_SECRET_ACCESS_KEY || "",
   },
 });
