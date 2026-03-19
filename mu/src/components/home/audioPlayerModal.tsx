1bee0b10a9ffec2a6b620b8e04beb9e76bf1e344
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2026-03-20 01:00:31 +0530

    Update audioPlayerModal.tsx

--- mu/src/components/home/audioPlayerModal.tsx
+++ mu/src/components/home/audioPlayerModal.tsx
@@ -584,7 +584,7 @@ function AudioPlayerModal({ id, handleId }: AudioPlayerModalProps) {
     };
 
     playExternalSong();
-  }, [id, fetchStreamAudioById]);
+  }, [id, currentAudioIndex, fetchStreamAudioById]);
 
   useEffect(() => {
     if (audioList.length > 0 && audioList[currentAudioIndex]) {
